abfce0151db9955f57d2078c697497a66b3797d5
Galian <fjavihortelano@gmail.com>
2026-02-09 21:35:29 +0100

    Initial structure

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function check(button) {
+  const exercise = button.parentElement;
+  const inputs = exercise.querySelectorAll("input");
+  const result = exercise.querySelector(".result");
+
+  const xCorrect = Number(exercise.dataset.x);
+  const yCorrect = Number(exercise.dataset.y);
+
+  const xUser = Number(inputs[0].value);
+  const yUser = Number(inputs[1].value);
+
+  if (xUser === xCorrect && yUser === yCorrect) {
+    result.textContent = "✅ Correcto";
+    result.className = "result ok";
+  } else {
+    result.textContent = "❌ Revisa la solución";
+    result.className = "result error";
+  }
+}
